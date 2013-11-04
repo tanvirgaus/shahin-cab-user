740b3610b5ca06651ba51f72abf82fcae73a1a4e
Tanvir Gaus <tanvir.gaus@gmail.com>
2013-11-04 22:36:18 +1100

    new design

--- js/application.js
+++ js/application.js
@@ -47,7 +47,11 @@ function watchCurrentPosition() {
 				currentPositionMarker,
 				position
 			);
-		});
+		},
+		'',
+		{frequency: 3000}
+		
+		);
 }
 
 function setMarkerPosition(marker, position) {
@@ -66,79 +70,10 @@ function initLocationProcedure() {
 		alert("Your browser does not support the Geolocation API");
 	}
 }
+function getCurrentPositon(){
+//	navigator.geolocation.getCurrentPosition(geolocationSuccess,[geolocationError],geolocationOptions]);
+}
 
 $(document).ready(function() {
 	initLocationProcedure();
 });
-
-
-/*
-function intializeMap(lat,long){
-
-var mapOptions = {
-center: new google.maps.LatLng(lat,long),
-zoom: 14,
-mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-var marker = new google.maps.Marker({
-position: new google.maps.LatLng(lat,long),
-map: map,
-title: 'My Location'
-});
-//return map;
-}
-var onSuccess = function(position) {
-//alert(position.coords.latitude);
-
-var t = intializeMap(position.coords.latitude, position.coords.longitude);
-google.maps.event.addDomListener(window, 'load', t);
-};
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-alert('code: '    + error.code    + '\n' +
-'message: ' + error.message + '\n');
-}
-
-function myLocation(){
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
-function loadMarker() {
-var lat = parseFloat(document.getElementById('markerLat').value);
-var lng = parseFloat(document.getElementById('markerLng').value);
-var newLatLng = new google.maps.LatLng(lat, lng);
-marker.setPosition(newLatLng);
-}
-
-
-
-
-myLocation();
-
-setInterval(function() { loadMarker(); },5000);
-*/
-/*
-function deviceready() {
-console.log("Device ready");
-navigator.compass.watchHeading(function(heading){
-	document.getElementById("heading").innerHTML = heading.trueHeading;
-},function(error){
-	var errorType;
-	switch(error.code){
-		case CompassError.COMPASS_NOT_SUPPORTED:
-			errorType = "Compass not supported";
-			break;
-		case CompassError.COMPASS_INTERNAL_ERR:
-			errorType = "Compass internal error";
-			break;
-		default:
-			errorType = "Unknown compass error";
-	}
-	document.getElementById("heading").innerHTML = errorType;
-});
-}
-document.addEventListener("deviceready", deviceready, true);
-*/
